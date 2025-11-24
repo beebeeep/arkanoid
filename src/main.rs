06146104f6baf48c96c36574eae14692deb6a1ee
Danila Migalin <me@miga.me.uk>
2025-11-24 21:20:37 +0200

    delaunay

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use rand::prelude::*;
 use std::time::Instant;
 
+use delaunator::{Point, triangulate};
 use raylib::prelude::*;
 
 const W: i32 = 1024;
@@ -16,6 +18,11 @@ struct Brick {
     hp: i32,
 }
 
+struct Shard {
+    edges: Vec<Vector2>,
+    hp: i32,
+}
+
 struct Pad {
     pos: Vector2,
     size: Vector2,
@@ -29,6 +36,7 @@ struct Ball {
 struct Game {
     pad: Pad,
     bricks: Vec<Brick>,
+    shards: Vec<Shard>,
     ball: Ball,
     last_update: Instant,
 }
@@ -82,54 +90,57 @@ impl Ball {
 
 impl Game {
     fn render(&self, rl: &mut RaylibHandle, thread: &RaylibThread) {
-        rl.draw(thread, |mut d| {
-            d.clear_background(Color::BLACK);
-            let p = &self.pad;
+        let mut d = rl.begin_drawing(thread);
+        d.clear_background(Color::BLACK);
+        let p = &self.pad;
 
-            d.draw_rectangle(
-                p.pos.x as i32,
-                p.pos.y as i32,
-                p.size.x as i32,
-                p.size.y as i32,
-                Color::LIGHTGREEN,
-            );
+        d.draw_rectangle(
+            p.pos.x as i32,
+            p.pos.y as i32,
+            p.size.x as i32,
+            p.size.y as i32,
+            Color::LIGHTGREEN,
+        );
 
-            for b in &self.bricks {
-                if b.hp < 1 {
-                    continue;
-                }
-                d.draw_rectangle(
-                    b.pos.x as i32,
-                    b.pos.y as i32,
-                    b.size.x as i32,
-                    b.size.y as i32,
-                    Color::RED,
-                );
-                d.draw_rectangle_lines(
-                    b.pos.x as i32,
-                    b.pos.y as i32,
-                    b.size.x as i32,
-                    b.size.y as i32,
-                    Color::DARKRED,
-                );
-                let txt = format!("{}", b.hp);
-
-                d.draw_text(
-                    &txt,
-                    (b.pos.x) as i32 + b.size.x as i32 / 2,
-                    (b.pos.y + b.size.y * 0.2) as i32,
-                    (b.size.y * 0.75) as i32,
-                    Color::YELLOW,
-                );
+        for b in &self.bricks {
+            if b.hp < 1 {
+                continue;
             }
-
-            d.draw_circle(
-                self.ball.pos.x as i32,
-                self.ball.pos.y as i32,
-                BALL_R,
-                Color::LIGHTBLUE,
+            d.draw_rectangle(
+                b.pos.x as i32,
+                b.pos.y as i32,
+                b.size.x as i32,
+                b.size.y as i32,
+                Color::RED,
             );
-        });
+            d.draw_rectangle_lines(
+                b.pos.x as i32,
+                b.pos.y as i32,
+                b.size.x as i32,
+                b.size.y as i32,
+                Color::DARKRED,
+            );
+            let txt = format!("{}", b.hp);
+
+            d.draw_text(
+                &txt,
+                (b.pos.x) as i32 + b.size.x as i32 / 2,
+                (b.pos.y + b.size.y * 0.2) as i32,
+                (b.size.y * 0.75) as i32,
+                Color::YELLOW,
+            );
+        }
+
+        d.draw_circle(
+            self.ball.pos.x as i32,
+            self.ball.pos.y as i32,
+            BALL_R,
+            Color::LIGHTBLUE,
+        );
+
+        for e in &self.shards {
+            d.draw_line_strip(&e.edges, Color::PINK);
+        }
     }
 
     fn update(&mut self, rl: &RaylibHandle) {
@@ -178,6 +189,31 @@ fn reflect(v: Vector2, n: Vector2) -> Vector2 {
 }
 
 fn main() {
+    let mut rng = rand::rng();
+    let mut points = Vec::with_capacity(100);
+    for _ in 0..points.capacity() {
+        points.push(Point {
+            x: rng.random_range(0.0..W as f64),
+            y: rng.random_range(0.0..(H / 3) as f64),
+        });
+    }
+    let mut tr = triangulate(&points).triangles.into_iter();
+    let mut shards = Vec::with_capacity(points.capacity());
+    loop {
+        if let (Some(p1), Some(p2), Some(p3)) = (tr.next(), tr.next(), tr.next()) {
+            shards.push(Shard {
+                edges: vec![
+                    Vector2::new(points[p1].x as f32, points[p1].y as f32),
+                    Vector2::new(points[p2].x as f32, points[p2].y as f32),
+                    Vector2::new(points[p3].x as f32, points[p3].y as f32),
+                ],
+                hp: 1,
+            });
+        } else {
+            break;
+        }
+    }
+
     let (mut rl, thread) = raylib::init().size(W, H).title("Arkanoid").build();
     let mut game = Game {
         last_update: Instant::now(),
@@ -186,6 +222,7 @@ fn main() {
             size: Vector2::new(PAD_W as f32, PAD_H as f32),
         },
         bricks: Vec::new(),
+        shards,
         ball: Ball {
             pos: Vector2::new(
                 (W / 2 + PAD_W / 2) as f32,
@@ -204,7 +241,7 @@ fn main() {
         }
     }
 
-    rl.set_target_fps(120);
+    rl.set_target_fps(60);
     rl.gui_lock();
     rl.disable_cursor();
     while !rl.window_should_close() {
